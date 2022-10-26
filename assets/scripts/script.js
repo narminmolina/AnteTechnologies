78d0f8908e9c6bf6e2016a7daa3b9d2cc6f40c51
Narmin Molina <narminmolina@gmail.com>
2022-10-26 00:25:46 -0400

    js preloader

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,77 @@
+const ready = (callback) => {
+  if (document.readyState != 'loading') callback();
+  else document.addEventListener('DOMContentLoaded', callback);
+};
+
+ready(() => {
+  let winPos,
+    winWidth,
+    winHeight,
+    navPos,
+    navHeight,
+    careersSec,
+    btnEndSec,
+    btn = document.querySelector('.btn-sticky');
+
+  let rect = document.querySelector('body').getBoundingClientRect();
+
+  var offset = {
+    top: rect.top + window.scrollY,
+    left: rect.left + window.scrollX,
+  };
+
+  function setVars() {
+    navPos = btn.offset;
+    // navHeight = btn.outerHeight(true);
+    // winPos = document.body.scrollTop;
+    winWidth = document.body.offsetWidth;
+    winHeight = document.body.offsetHeight;
+    let buttonEnd = document.querySelector('.btn-sticky-end');
+    console.log(winPos);
+
+    // if (document.querySelector('.our-options').length > 0) careersSec = $('.our-options').offset.top;
+
+    // if (swiperExists) swiperSec = $('#' + swiperId).offset().top;
+  }
+
+  // **********************
+  // Preloader ************
+  // **********************
+
+  // $('html').addClass('loaded');
+
+  setTimeout(function () {
+    // Hide preloader
+    document.querySelector('.preloader').style.visibility = 'hidden';
+
+    // $('.flying-person-1, .flying-person-2').addClass('flying-person-anime');
+    // Update variables
+    setVars();
+  }, 2000);
+
+  // **********************
+  // Sticky Button ************
+  // **********************
+
+  setVars();
+
+  let button = document.body.querySelector('.btn-sticky');
+
+  window.addEventListener('scroll', stickButton);
+
+  function stickButton() {
+    if (winPos > 0 && winPos < buttonEnd.offsetHeight && winWidth >= 992) {
+      button.classList.add('fixed', 'active');
+      document.querySelector('.clone-btn-sticky').style.display = 'inline-block';
+    } else {
+      button.classList.remove('fixed', 'active');
+    }
+    // button.trigger('mouseenter');
+
+    // $('.clone-btn-sticky').hide();
+  }
+});
+
 // **********************
 // WOW.js ***************
 // **********************
