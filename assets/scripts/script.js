d2bf8d4fbcb0445fc4871ca887cb68d453e5d357
Narmin Molina <narminmolina@gmail.com>
2022-10-29 22:42:40 -0400

    API

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -101,3 +101,140 @@ let wowAnime = new WOW({
   mobile: false,
 });
 wowAnime.init();
+
+// **********************
+// Swiper ***************
+// **********************
+
+let swipersAll = ['homeSwiper', 'aboutSwiper', 'worksSwiper'],
+  swiperId,
+  swiperSec,
+  swiperExists = false,
+  swiperInView = false,
+  swiper,
+  winWidth;
+
+for (let i = 0; i < swipersAll.length; i++) {
+  if (document.querySelector('#' + swipersAll[i])) {
+    swiperExists = true;
+    swiperId = swipersAll[i];
+  }
+}
+if (swiperExists) {
+  swiper = new Swiper('#' + swiperId, {
+    initialSlide: 2,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: document.querySelectorAll('.swiper-slide').length,
+    speed: 1000,
+    autoplay: {
+      delay: 8000,
+      waitForTransition: false,
+      disableOnInteraction: false,
+    },
+    simulateTouch: winWidth >= 992 ? false : true,
+    // keyboard: true,
+    pagination: {
+      clickable: true,
+      el: '.swiper-dots',
+      bulletClass: 'swiper-dot',
+      bulletActiveClass: 'active',
+      modifierClass: 'swiper-dot-',
+      renderBullet: function (index, className) {
+        let bulletNum = index++ < 10 ? '0' + index++ : index++;
+        return `
+          <div class="${className}">
+            <span class="number">${bulletNum}</span>
+            <svg><circle class="outer" cx="35" cy="35" r="33"/></svg>
+            <span class="bullet"></span>
+          </div>
+        `;
+      },
+    },
+  });
+
+  setTimeout(function () {
+    swiper.slideTo(1);
+  }, 1000);
+
+  // $(window).on('scroll', function () {
+  //   // Return to initial slide for the first time
+  //   if (swiperExists && !swiperInView && winPos > swiperSec - winHeight) {
+  //     swiperInView = true;
+  //     swiper.slideTo(0);
+  //   }
+  // });
+
+  // swiper.on('slideChangeTransitionStart', function () {
+  //   $('.swiper-dot.active').siblings().removeClass('tick-tack');
+  //   $('.swiper-dot.active').addClass('tick-tack');
+
+  //   if (swiper.isEnd) $('.swiper-start').addClass('active');
+  //   else $('.swiper-start').removeClass('active');
+  // });
+
+  let lastSlideId = swiper.slides.length - 1;
+
+  // $('.swiper-nav-arrow').on('click touchstart', function () {
+  //   let $arrow = $(this),
+  //     prevSlideId,
+  //     nextSlideId,
+  //     parentSlideId;
+
+  //   parentSlideId = $arrow.parent('.swiper-slide').index();
+  //   (prevSlideId = swiper.realIndex == 0 ? lastSlideId : swiper.realIndex - 1), (nextSlideId = swiper.realIndex == lastSlideId ? 0 : swiper.realIndex + 1);
+
+  //   switch ($arrow.data('to')) {
+  //     default:
+  //     case 'start':
+  //       swiper.slideTo(0);
+  //       break;
+  //     case 'prev':
+  //       swiper.slideTo(prevSlideId);
+  //       break;
+  //     case 'next':
+  //       swiper.slideTo(nextSlideId);
+  //       break;
+  //     case 'slide':
+  //       swiper.slideTo(parentSlideId);
+  //       break;
+  //   }
+  // });
+}
+
+//  API Integration:
+
+const container = document.querySelector('.vacancies');
+
+async function getEpisode() {
+  const response = await fetch('https://swapi.dev/api/films/', {
+    method: 'GET',
+  });
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+async function applyCardInfo(results) {
+  let data = await getEpisode();
+
+  container.innerHTML = '';
+  data.results.forEach((obj) => {
+    let title = obj.title;
+    let location = obj.producer;
+    let id = obj.episode_id;
+    container.innerHTML += `
+ <div class="job" data-department="engineering" data-location="dublin">
+        <h2>
+         <a href="careers-inner.html#current-position-anchor">${title}</a>
+      </h2>
+        <address>${location}</address>
+        <a href="careers-inner.html#${id}">Apply Now</a>
+       </div>`;
+  });
+}
+
+
+
+applyCardInfo();
